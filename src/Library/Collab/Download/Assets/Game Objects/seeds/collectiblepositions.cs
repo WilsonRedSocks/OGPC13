ffc89888659bc68b44a3f848de8ae20a640db37b
P3G45U5 <P.3G45U5@outlook.com>
2020-05-13 15:08:40 -0700

    commit

---
+++ src/Library/Collab/Download/Assets/Game Objects/seeds/collectiblepositions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System;
+
+public class collectiblepositions : MonoBehaviour
+{
+    public List<Vector3> positions;
+
+    public GameObject seed;
+
+    public List<GameObject> seeds;
+
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.root.gameObject);
+        positions = positions.OrderBy(x => Random.value).ToList();
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(seed, positions[i], Quaternion.identity);
+        }
+        Destroy(gameObject);
+    }
+}
